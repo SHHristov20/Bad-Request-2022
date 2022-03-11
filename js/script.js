f641940716ee77ab874045de30648393f35f1f06
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-03-11 20:03:06 +0200

    Implement revealing on scroll for summary section (fix #11)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+const allSections = document.querySelectorAll(".section");
+
+allSections.forEach((section) => {
+  section.classList.remove("section--hidden");
+});
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  //   console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
